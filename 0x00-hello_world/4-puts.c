373a0764da4ba15765d84da2e6d15b46db7360e4
Kennedy Simiyu <mukhebisimiyu@gmail.com>
2022-03-10 04:30:04 -0800

    4-puts corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* My first program in C */
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
