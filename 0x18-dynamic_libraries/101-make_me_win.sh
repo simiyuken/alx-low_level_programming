c8af50b3614aa65a2ed7fdc9f9440a535290352c
Kennedy Simiyu <mukhebisimiyu@gmail.com>
2022-06-13 11:50:49 -0700

    trial_2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P .. https://github.com/simiyuken/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+export LD_PRELOAD="$PWD/../libmask.so"
