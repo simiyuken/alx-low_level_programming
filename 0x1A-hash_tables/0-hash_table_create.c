da5d410d2924850c956a95abccded7852bdb3da9
Kennedy Simiyu <mukhebisimiyu@gmail.com>
2022-06-30 10:23:36 -0700

    fixing

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_create - create a hash table
  * @size: the size of hash table
  *
  * Return: returns the hash table
  */
+
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
